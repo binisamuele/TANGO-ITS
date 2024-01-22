4e6a71204d82ce3671ae6d934d4ea169e69c21c9
GennaIts <122350562+GennaIts@users.noreply.github.com>
2024-01-22 10:24:36 +0100

    mapping

--- Controllo/Controllo.ino
+++ Controllo/Controllo.ino
@@ -1,5 +1,6 @@
 int emergencyControl;
 String movement = "";
+int movementInt;
 int a1 = 2, a2 = 3, enA = 9;  // Motore A SX
 int b1 = 4, b2 = 5, enB = 10; // Motore B DX
 
@@ -22,37 +23,36 @@ void loop(){
         if(emergencyControl == 1){              //Se c'è un'emergenza
                                                 //codice per fermare il motore
             Serial.write("");
-            break;
         }
 
-        movement=Serial.read();
-        switch(movement){
-          case "left":
-            leftMotor(enA, a1, a2, enB, b1, b2);
-            break;
-
-          case "right":
-            rightMotor(enA, a1, a2, enB, b1, b2);
-            break;
-
-          case "forward":
+        mapping();
+        switch(movementInt){
+          case 1:
             driveMotor(enA, a1, a2);
             driveMotor(enB, b1, b2);
             delay(2000);
             break;
 
-          case "backward":
+          case 2:
             reverseMotor(enA, a1, a2);
             reverseMotor(enB, b1, b2);
             delay(2000);
             break;
 
-          case "rotateSX":
+          case 3:
+            leftMotor(enA, a1, a2, enB, b1, b2);
+            break;
+
+          case 4:
+            rightMotor(enA, a1, a2, enB, b1, b2);
+            break;
+            
+          case 5:
             driveMotor(enA, a1, a2);
             reverseMotor(enB, b1, b2);
             break;
 
-          case "rotateDX":
+          case 6:
             driveMotor(enB, b1, b2);
             reverseMotor(enA, a1, a2);
             break;
@@ -61,6 +61,15 @@ void loop(){
     }
 }
 
+void mapping(){
+    movement=Serial.read();
+    if(movement == "forward")movementInt=1;
+    if(movement == "backward")movementInt=2;
+    if(movement == "left")movementInt=3;
+    if(movement == "right")movementInt=4;
+    if(movement == "rotateSX")movementInt=5;
+    if(movement == "rotateDX")movementInt=6;
+}
 
 void emergencyStop() {
   digitalWrite(a1, LOW);
