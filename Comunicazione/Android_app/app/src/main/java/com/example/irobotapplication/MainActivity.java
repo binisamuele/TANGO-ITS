889db907a2594c1c152ebf16bc96dfcb159163c7
R4vec <francesco.ravecca.12-1@accademiadigitaleliguria.it>
2024-01-22 12:40:18 +0100

    Menu app

--- Comunicazione/Android_app/app/src/main/java/com/example/irobotapplication/MainActivity.java
+++ Comunicazione/Android_app/app/src/main/java/com/example/irobotapplication/MainActivity.java
@@ -1,14 +1,80 @@
 package com.example.irobotapplication;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
+
+    DrawerLayout drawerLayout;
+    Toolbar toolbar;
+    NavigationView navigationView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        toolbar=findViewById(R.id.toolbar);
+        drawerLayout=findViewById(R.id.drawerLayout);
+        navigationView=findViewById(R.id.nav);
+
+        ActionBarDrawerToggle toggle=new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.open, R.string.close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+        toggle.getDrawerArrowDrawable().setColor(getResources().getColor(R.color.white));
+
+
+
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                int itemId = item.getItemId();
+                if (itemId == R.id.home) {
+                    drawerLayout.closeDrawer(GravityCompat.START);
+                    Toast.makeText(MainActivity.this, "Home", Toast.LENGTH_SHORT).show();
+                    fragmentR(new HomeFragment());
+                } else if (itemId == R.id.controller) {
+                    fragmentR(new ControllerFragment());
+                    drawerLayout.closeDrawer(GravityCompat.START);
+                    Toast.makeText(MainActivity.this, "Controller", Toast.LENGTH_SHORT).show();
+                } else if (itemId == R.id.lidar) {
+                    fragmentR(new LidarFragment());
+                    drawerLayout.closeDrawer(GravityCompat.START);
+                    Toast.makeText(MainActivity.this, "Lidar", Toast.LENGTH_SHORT).show();
+                }
+                else if (itemId == R.id.telecamere) {
+                    fragmentR(new FotocamereFragment());
+                    drawerLayout.closeDrawer(GravityCompat.START);
+                    Toast.makeText(MainActivity.this, "Telecamere", Toast.LENGTH_SHORT).show();
+                }
+
+                return true;
+            }
+        });
+        if (savedInstanceState == null) {
+            fragmentR(new HomeFragment());
+            navigationView.setCheckedItem(R.id.home); // Imposta l'elemento di menu Home come selezionato
+        }
     }
+    private void fragmentR(Fragment fragment){
+        FragmentManager fragmentManager=getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction=fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frameLayout, fragment);
+        fragmentTransaction.commit();
+    }
+    
 }
