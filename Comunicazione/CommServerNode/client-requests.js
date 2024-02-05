f31a75f894516be511192732b268a411bf060995
Samuele Bini <91492171+binisamuele@users.noreply.github.com>
2024-02-05 13:22:36 +0100

    Update client-requests.js

--- Comunicazione/CommServerNode/client-requests.js
+++ Comunicazione/CommServerNode/client-requests.js
@@ -1,1 +1,45 @@
-created
+forwardToArduino = (direction) => {
+    if (!isValidDirection(direction)){
+        console.log('Invalid direction. Not forwarding to Arduino.');
+        return;
+    }
+
+    if (direction === lastDirection) {
+        console.log('Direction is the same as the previous one. Not forwarding to Arduino.');
+        return;
+    }
+
+    lastDirection = direction;
+
+    const jsonData = {
+        direction: direction
+    };
+
+    const options = {
+        hostname: arduinoHost,
+        port: arduinoPort,
+        path: '', 
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Content-Length': JSON.stringify(jsonData).length
+        }
+    };
+
+    const req = http.request(options, (res) => {
+        console.log(`Arduino server responded with status code: ${res.statusCode}`);
+    });
+
+    req.on('error', (error) => {
+        console.error('Error sending request to Arduino:', error);
+    });
+
+    req.write(JSON.stringify(jsonData));
+    req.end();
+};
+
+isValidDirection = (direction) => {
+    const validDirections = ['up', 'down', 'left', 'right', "released", "emergencyStop"];
+    return validDirections.includes(direction);
+};
+
