c862653b8c9191e3b53c6cd0ae673101e7fb6341
R4vec <francesco.ravecca.12-1@accademiadigitaleliguria.it>
2024-02-13 11:51:02 +0100

    Modifica Home

--- Comunicazione/Android_app/app/src/main/java/com/example/irobotapplication/HomeFragment.java
+++ Comunicazione/Android_app/app/src/main/java/com/example/irobotapplication/HomeFragment.java
@@ -1,31 +1,24 @@
 package com.example.irobotapplication;
 
-import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+// HomeFragment.java
 public class HomeFragment extends Fragment {
 
-    public HomeFragment() {
-        // Required empty public constructor
-    }
 
+    @Nullable
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        requireActivity().setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_home, container, false);
 
-        View view = inflater.inflate(R.layout.fragment_home, container, false);
 
-        // Accedi a TemperatureView e imposta i valori della temperatura e dell'umidità
-        TemperatureView temperatureView = view.findViewById(R.id.temperatureView);
-        temperatureView.setTemperature("25°C");
-        temperatureView.setHumidity("50%");
-
-        return view;
+        return rootView;
     }
 }
