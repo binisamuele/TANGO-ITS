6bdf6d8d0c4df0964d87aec583e472c61f1e5e0e
GionataBurioni <135854516+GionataBurioni@users.noreply.github.com>
2024-02-16 12:33:42 +0100

    Update LoadingFragment.java
    gionata

--- Comunicazione/Android_app/app/src/main/java/com/example/irobotapplication/LoadingFragment.java
+++ Comunicazione/Android_app/app/src/main/java/com/example/irobotapplication/LoadingFragment.java
@@ -1,5 +1,6 @@
 package com.example.irobotapplication;
 
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.LayoutInflater;
@@ -12,29 +13,17 @@ import androidx.fragment.app.Fragment;
 
 public class LoadingFragment extends Fragment {
 
-    private LoadingListener loadingListener;
-
-    public void setLoadingListener(LoadingListener listener) {
-        this.loadingListener = listener;
-    }
-
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        // Implementa la UI del tuo fragment di caricamento
-        // ...
 
-        // Simula il completamento del caricamento dopo un certo tempo
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                // Notifica la MainActivity che il caricamento è completato
-                if (loadingListener != null) {
-                    loadingListener.onLoadingFinished();
-                }
-            }
-        }, 3000); // Simula il caricamento per 3 secondi
+        View loadingView = inflater.inflate(R.layout.fragment_loading, container, false);
 
-        return super.onCreateView(inflater, container, savedInstanceState);
+        return loadingView;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        requireActivity().setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
     }
 }
