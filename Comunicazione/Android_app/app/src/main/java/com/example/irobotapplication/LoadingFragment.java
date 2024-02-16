10c5a8c002acff1c63a1716d74395e58599045e9
binisamuele <samuele@bini.me.it>
2024-02-16 12:39:22 +0100

    Update app

--- Comunicazione/Android_app/app/src/main/java/com/example/irobotapplication/LoadingFragment.java
+++ Comunicazione/Android_app/app/src/main/java/com/example/irobotapplication/LoadingFragment.java
@@ -9,51 +9,15 @@ import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
+import androidx.appcompat.app.AppCompatActivity;
 
-public class LoadingFragment extends Fragment {
-
-    private LoadingListener loadingListener;
-
+public class LoadingFragment extends AppCompatActivity {
     private ConnectToArduino connect;
 
-    public void setLoadingListener(LoadingListener listener) {
-        this.loadingListener = listener;
-    }
-
-    @Nullable
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        // Implementa la UI del tuo fragment di caricamento
-        // ...
-
-        // Simula il completamento del caricamento dopo un certo tempo
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                // Notifica la MainActivity che il caricamento è completato
-                if (loadingListener != null) {
-                    loadingListener.onLoadingFinished();
-                }
-            }
-        }, 3000); // Simula il caricamento per 3 secondi
-
-        return super.onCreateView(inflater, container, savedInstanceState);
-    }
-
-    @Override
-    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
-
-        requireActivity().setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
-
-        connect = new ConnectToArduino();
-        do {
-            // prova a connetterti all'Arduino e scamabia l'IP
-            GlobalVars.arduinoIP = connect.tryConnection();
-        } while (GlobalVars.arduinoIP == null);
-
-        // chiudi il fragment
-        requireActivity().getSupportFragmentManager().beginTransaction().remove(this).commit();
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_loading);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
     }
 }
