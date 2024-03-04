66c83b6c90d728506901275ff4df24de3c1401a8
binisamuele <samuele@bini.me.it>
2024-03-04 10:43:08 +0100

    Update ControllerFragment.java

--- Comunicazione/Android_app/app/src/main/java/com/example/irobotapplication/ControllerFragment.java
+++ Comunicazione/Android_app/app/src/main/java/com/example/irobotapplication/ControllerFragment.java
@@ -20,7 +20,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Switch;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.android.volley.DefaultRetryPolicy;
 import com.android.volley.Request;
